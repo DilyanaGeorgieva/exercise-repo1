1ab22a682bda96f31bb603a0e13546201f314fbd
DilyanaGeorgieva <miss_djd@abv.bg>
2017-05-21 13:41:04 +0300

    new program

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace start1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write(num);
+                        Console.Write(" ");
+                        num++;
+                    }
+                    if (num > n)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+                if (num > n)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
